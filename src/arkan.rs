6289a25f63a673d6c31e961ed3e9aa9773cce316
himozzza <89920794+himozzza@users.noreply.github.com>
2023-07-04 15:30:13 +0300

    new regex and date pattern verification

--- src/arkan.rs
+++ src/arkan.rs
@@ -1,18 +1,24 @@
 pub(crate) async fn arkan(msg: String) -> (String, String) {
-    let arkan_array: [&str; 22] = ["Маг", "Верховная жрица", "Императрица", "Император", "Верховный жрец", "Влюбленные", "Колесница", "Правосудие", "Отшельник", "Колесо фортуны", "Сила", "Повешенный", "Смерть", "Умеренность", "Дьявол", "Башня", "Звезда", "Луна", "Солнце", "Суд", "Мир", "Шут"];
-    let date_raw = msg.replace(".", "");
-    let mut arkan_two_raw: usize = 0;
-    for i in date_raw.chars() {
-        arkan_two_raw += i.to_string().parse::<usize>().unwrap();
+    let mut arkan_one: String = String::new();
+    let mut arkan_two: String = String::new();
+    let verify_date: Vec<usize> = msg.split(".").map(|f| f.parse::<usize>().unwrap()).collect();
+    if verify_date[0] <= 31 && verify_date[1] <= 12 {
+        let arkan_array: [&str; 22] = ["Маг", "Верховная жрица", "Императрица", "Император", "Верховный жрец", "Влюбленные", "Колесница", "Правосудие", "Отшельник", "Колесо фортуны", "Сила", "Повешенный", "Смерть", "Умеренность", "Дьявол", "Башня", "Звезда", "Луна", "Солнце", "Суд", "Мир", "Шут"];
+        let date_raw = msg.replace(".", "");
+        let mut arkan_two_raw: usize = 0;
+        for i in date_raw.chars() {
+            arkan_two_raw += i.to_string().parse::<usize>().unwrap();
+        }
+        let mut arkan_one_raw = date_raw[0..2].to_string().parse::<usize>().unwrap();
+        if arkan_one_raw > 22 {
+            arkan_one_raw -= arkan_one_raw - 22
+        }
+        if arkan_two_raw > 22 {
+            arkan_two_raw = &arkan_two_raw - 22
+        }
+        arkan_one = arkan_array[arkan_one_raw - 1].to_string();
+        arkan_two = arkan_array[arkan_two_raw - 1].to_string();
+
     }
-    let mut arkan_one_raw = date_raw[0..2].to_string().parse::<usize>().unwrap();
-    if arkan_one_raw > 22 {
-        arkan_one_raw -= arkan_one_raw - 22
-    }
-    if arkan_two_raw > 22 {
-        arkan_two_raw = &arkan_two_raw - 22
-    }
-    let arkan_one: &str = arkan_array[arkan_one_raw - 1];
-    let arkan_two: &str = arkan_array[arkan_two_raw - 1];
     (arkan_one.to_string(), arkan_two.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,12 @@ async fn main() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-            let re: Regex = Regex::new("[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}").unwrap();
+            let re: Regex = Regex::new("[0-3]{0,1}[0-9]{1}.[0-1]{0,1}[0-2]{1}.[0-9]{4}").unwrap();
             if re.is_match(msg.text().unwrap()) {
-                let (arkan_one, arkan_two) = arkan::arkan(msg.text().unwrap().to_string()).await;
-                let s: String = format!("Ваши арканы:\n1. {}.\n2. {}.", arkan_one, arkan_two);
-                bot.send_message(msg.chat.id, s).await?;
+                    let (arkan_one, arkan_two) = arkan::arkan(msg.text().unwrap().to_string()).await;
+                    let s: String = format!("Ваши арканы:\n1. {}.\n2. {}.", arkan_one, arkan_two);
+                    bot.send_message(msg.chat.id, s).await?;
+                
             } else {
                 let q: String = forecast::get_forecast(msg.text().unwrap().to_string()).await;
                 bot.send_message(msg.chat.id, q).await?;
